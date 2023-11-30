2dc88da69512e6c24c12ade536e6ab0a9380263c
S0506 <S0506@21S0506>
2023-11-30 09:51:08 +0900

    あむろ

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+
+	}
+
+static void kadaiA() {
+	 //問題
+for(int i=0;i<10;i++) {
+	System.out.println("現在"+i+"回目です");
+}
+
+
+ }
+}
